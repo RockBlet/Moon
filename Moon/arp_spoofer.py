03f4619a75d25c13b5e5d2ee8c4f9c47ce5e75cc
RockBlet <velyakove31@gmail.com>
2022-12-16 23:20:47 +0300

    add packet create function, add name main struct

--- Moon/arp_spoofer.py
+++ Moon/arp_spoofer.py
@@ -13,10 +13,14 @@ def get_arguments():
     return options
 
 
-options = get_arguments()
-target_ip = options.target_ip
-route_ip = options.route_ip
-target_mac = options.target_mac
+def packets_create(options):
 
-packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=route_ip)
+    target_ip = options.target_ip
+    route_ip = options.route_ip
+    target_mac = options.target_mac
 
+    target_packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=route_ip)
+
+
+if __name__ == "__main__":
+    pass
